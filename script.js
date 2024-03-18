3a5c819be20e4a789f0aa71898d6e21f84f629ad
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-18 10:36:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let submit = document.getElementById('btn');
+submit.addEventListener('click',()=>{
+  let name = document.getElementById('name').value;
+  let age = document.getElementById('age').value;
+  //console.log(name, age);
+  
+  let promise = new Promise(function(resolve, reject){
+    if (age >= 18){
+      resolve(`Welcome, ${name} You can vote.`);
+    }
+    else{
+      reject(`Oh sorry ${name}. You aren't old enough.`);
+    }
+  });
+
+  promise.then(message => {
+    window.alert(message);
+  }).catch(error => {
+    window.alert(error);
+  });
+});
